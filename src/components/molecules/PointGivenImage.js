4cbf3c8b437439043069ad0645e84a7e0a73e03a
heondo kim <heondokim@gmail.com>
2021-01-28 20:27:11 -0800

    way better image pan zoom library

--- src/components/molecules/PointGivenImage.js
+++ src/components/molecules/PointGivenImage.js
@@ -2,21 +2,36 @@ import React from 'react'
 import { Dimensions } from 'react-native'
 import { connect } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
-import ImageZoom from 'react-native-image-pan-zoom'
-import { Image, PointImageContainer } from '../atoms'
+import ImageViewer from 'react-native-image-zoom-viewer'
+
+import { Image, PointImageContainer, View } from '../atoms'
 
 const PointGivenImageComponent = ({ navigation, theme, route, image }) => {
   const { width: deviceWidth, height: deviceHeight } = Dimensions.get('screen')
+  const images = [
+    // {
+    //   url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460',
+    // },
+    {
+      // url: 'https://avatars2.githubusercontent.com/u/7970947?v=3&s=460',
+      // url: '',
+      url: '',
+      props: { source: image },
+    },
+  ]
   return (
     <ThemeProvider theme={theme}>
-      <ImageZoom
+      <View height="100%" width="auto">
+        <ImageViewer imageUrls={images} />
+      </View>
+      {/* <ImageZoom
         cropWidth={deviceWidth}
         cropHeight={deviceHeight}
         imageHeight={deviceWidth}
         width={deviceHeight}
       >
         <Image width="100%" height="98%" resizeMode="contain" source={image} />
-      </ImageZoom>
+      </ImageZoom> */}
       {/* <PointImageContainer>
         <Image width="100%" height="98%" resizeMode="contain" source={image} /> */}
       {/* <PointImages /> */}
