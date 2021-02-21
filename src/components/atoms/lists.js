fb787d979488a8fb104a7d5b57396379df77109d
heondo kim <heondokim@gmail.com>
2021-02-21 13:01:02 -0800

    list styling

--- src/components/atoms/lists.js
+++ src/components/atoms/lists.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components/native'
-import { View } from './base'
+import { FlatList, View } from './base'
 
 export const ListItemContainer = styled(View)`
   padding: ${(props) => (props.pd ? props.pd : '16px 6px')};
   border-style: solid;
   border-bottom-color: lightgrey;
   border-bottom-width: 1px;
+`
+
+export const SearchList = styled(FlatList)`
+  width: 100%;
+  padding: 4px 8px;
 `
